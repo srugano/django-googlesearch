07d5a306a097d929b3f8a9bf174423bb09a5b6c5
RUGANO <srugano@users.noreply.github.com>
2014-06-22 13:01:55 +0200

    fixed direct to template deprecation

--- googlesearch/urls.py
+++ googlesearch/urls.py
@@ -3,10 +3,7 @@ version = django.get_version()
 
 if version < 1.6:
     from django.conf.urls.defaults import patterns, url
-else:
-    from django.conf.urls import patterns, url
-
-urlpatterns = patterns(
+    urlpatterns = patterns(
     '',
 
     url(
@@ -14,6 +11,24 @@ urlpatterns = patterns(
         'django.views.generic.simple.direct_to_template', 
         {'template': 'googlesearch/results.html'},
         name='googlesearch-results'
+    ),
+
+    url(
+        r'^cref-cse\.xml/$', 
+        'googlesearch.views.cref_cse', 
+        {},
+        name='googlesearch-cref-cse'
+    ),
+)
+else:
+    from django.conf.urls import patterns, url
+    from django.views.generic import TemplateView
+
+    urlpatterns = patterns(
+    '',
+
+    url(
+        r'^results/$', TemplateView.as_view(template_name='googlesearch/results.html'), name="googlesearch-results"),
     ),
 
     url(
